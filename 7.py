ce3b23f8fd1d11a50b8b8a38c34e910b434cdbe8
Joel Heinerud <joel.heinerud@gmail.com>
2020-12-07 17:22:13 +0100

    Day 7: Handy Haversacks

---
+++ 7.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+
+def contains(rules, bag):
+    hits = set()
+    for k, v in rules.items():
+        if bag in [b for _, b in v]:
+            hits.add(k)
+            hits |= contains(rules, k)
+
+    return hits
+
+
+def num_bags(rules, bag):
+    total = 0
+    for n, inner in rules[bag]:
+        total += n
+        total += n * num_bags(rules, inner)
+
+    return total
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as f:
+        lines = [x.strip() for x in f.readlines()]
+
+    rules = {}
+    for l in lines:
+        bag, contents = l.split(" bags contain ")
+        contents = re.findall("(\d+) (\S+ \S+)", contents)
+        contents = [(int(n), b) for n, b in contents]
+        rules[bag] = contents
+
+    print(len(contains(rules, "shiny gold")))
+    print(num_bags(rules, "shiny gold"))
